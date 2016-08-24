4fb89a31f206b4d67ae2d2da2a69305c59c9202f
srbutler <srbutler@gmail.com>
2016-08-24 09:34:07 -0400

    different computer different things

--- custom.el
+++ custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (clj-refactor yaml-mode wrap-region which-key use-package solarized-theme smartparens skewer-mode rainbow-delimiters pyenv-mode pandoc-mode pallet org-plus-contrib mmm-mode markdown-mode magit helm-projectile helm-descbinds helm-ag flycheck-pos-tip flycheck-hdevtools flycheck-haskell flycheck-clojure flycheck-cask exec-path-from-shell ess emmet-mode elpy dash-at-point cython-mode company-ghc coffee-mode avy))))
+ '(org-todo-keywords (quote ((sequence "TODO" "DONE" "|" "CANCELLED" "MAYBE")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -5,13 +5,20 @@
 ;;; Code:
 
 ;; cask/pallet setup
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
 
 ;; go to debug on error
-(setq debug-on-error t)
+;; (setq debug-on-error t)
 
 ;; Always load newest byte code
 (setq load-prefer-newer +1)
