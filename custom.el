ab4ae7a8168e1ae70f5b22eeb8d92bc40f4ff10e
srbutler <srbutler@gmail.com>
2016-08-24 18:02:20 -0400

    package updates

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (pcre2el zenburn-theme yaml-mode wrap-region which-key use-package solarized-theme smartparens slime skewer-mode rainbow-mode rainbow-delimiters pyenv-mode pandoc-mode pallet org-plus-contrib mmm-mode material-theme markdown-mode magit helm-projectile helm-descbinds helm-ag git-gutter flycheck-pos-tip flycheck-hdevtools flycheck-haskell flycheck-clojure flycheck-cask exec-path-from-shell ess emmet-mode elpy dash-at-point darkokai-theme cython-mode company-ghc color-theme-sanityinc-tomorrow coffee-mode clj-refactor cdlatex avy))))
+    (key-chord zenburn-theme yaml-mode wrap-region which-key use-package solarized-theme smartparens slime skewer-mode rainbow-mode rainbow-delimiters pyenv-mode pcre2el pandoc-mode pallet org-plus-contrib mmm-mode material-theme markdown-mode magit helm-projectile helm-descbinds helm-ag git-gutter flycheck-pos-tip flycheck-hdevtools flycheck-haskell flycheck-clojure flycheck-cask exec-path-from-shell ess emmet-mode elpy dash-at-point darkokai-theme cython-mode company-ghc color-theme-sanityinc-tomorrow coffee-mode clj-refactor cdlatex avy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
