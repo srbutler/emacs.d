d9666db736e2c24eda109d3fd0a87ab88619b769
Steven Butler <srbutler@gmail.com>
2016-09-06 20:14:39 -0400

    Updates to lang-haskell

--- custom.el
+++ custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
+ '(package-selected-packages
    (quote
-    ("e56ee322c8907feab796a1fb808ceadaab5caba5494a50ee83a13091d5b1a10c" default))))
+    (company-ghci zenburn-theme yaml-mode wrap-region which-key web-mode use-package solarized-theme smartparens smart-mode-line slime skewer-mode rustfmt rainbow-mode rainbow-delimiters racer pyenv-mode pcre2el pandoc-mode pallet org-plus-contrib mmm-mode material-theme markdown-mode magit leuven-theme key-chord json-mode js2-refactor js-comint intero helm-projectile helm-descbinds helm-ag git-gutter flycheck-rust flycheck-pos-tip flycheck-hdevtools flycheck-haskell flycheck-clojure flycheck-cask exec-path-from-shell ess emmet-mode elpy elixir-mode dash-at-point darkokai-theme cython-mode csv-mode company-tern company-racer company-ghc color-theme-sanityinc-tomorrow coffee-mode clj-refactor cdlatex cargo avy auctex anti-zenburn-theme))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- modules/lang-haskell.el
+++ modules/lang-haskell.el
@@ -8,68 +8,41 @@
   :ensure t
   :mode (("\\.hs\\'" . haskell-mode)
          ("\\.cabal$". haskell-mode))
- 
   :init
   (add-hook 'haskell-mode-hook 'subword-mode)
-  (add-hook 'haskell-mode-hook 'flycheck-mode)
-  (add-hook 'haskell-mode-hook 'wrap-region-mode)
-  (add-hook 'haskell-mode-hook 'electric-pair-mode)
   (add-hook 'haskell-mode-hook 'haskell-doc-mode)
   (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
   (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-  
+
   :config
-  (setq haskell-process-type 'stack-ghci
-        haskell-process-suggest-remove-import-lines  t
+  (setq 
+        haskell-compile-cabal-build-command "stack build"
+        haskell-font-lock-symbols nil
+        haskell-interactive-mode-scroll-to-bottom t
         haskell-process-auto-import-loaded-modules t
+        haskell-process-load-or-reload-prompt t
         haskell-process-log t
+        haskell-process-suggest-remove-import-lines  t
+        haskell-process-type 'stack-ghci
+        ;; haskell-process-type 'auto
         haskell-stylish-on-save t
-        ;; haskell-font-lock-symbols t
-        )
+        haskell-completions-complete-operators t
+        ;; haskell-notify-p t
+        ;; haskell-doc-show-reserved nil
+        ;; haskell-indent-spaces 4
+        ;; haskell-indent-offset 4
+        ;; haskell-doc-show-global-types t
+      )
 
-  (bind-keys :map haskell-mode-map
-             ;; Indent the below lines on columns after the current column.
-             ("C-,"     . haskell-move-nested-left)
-             ;; Same as above but backwards.
-             ("C-."     . haskell-move-nested-right)
-             ("C-c C-." . haskell-mode-format-imports)
-             ;; Switch to the REPL.
-             ("C-c C-z" . haskell-interactive-switch)
-             ;; “Bring” the REPL, hiding all other windows apart from
-             ;; the source and the REPL.
-             ("C-`"     . haskell-interactive-bring)
-             ;; Load the current file (and make a session if not
-             ;; already made).
-             ("C-c C-l" .  haskell-process-load-or-reload)
-             ;; Get the type and info of the symbol at point, print it
-             ;; in the message buffer.
-             ("C-c C-t" . haskell-process-do-type)
-             ("C-c C-i" . haskell-process-do-info)
-             ;; Build the Cabal project.
-             ("C-c C-c" . haskell-process-cabal-build)
-             ("C-c C-k" . haskell-interactive-mode-clear)
-             ("C-c c"   . haskell-process-cabal)
-             )
-
-  ;; Indent the below lines on columns after the current column.
-  (define-key haskell-mode-map (kbd "C-<right>")
-    (lambda ()
-      (interactive)
-      (haskell-move-nested 1)))
-  ;; Same as above but backwards.
-  (define-key haskell-mode-map (kbd "C-<left>")
-    (lambda ()
-      (interactive)
-      (haskell-move-nested -1)))
-            
-
-  ;; Useful to have these keybindings for .cabal files, too.
-  (defun haskell-cabal-hook ()
-    (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-    (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
-    (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
-    (define-key haskell-cabal-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch))
   )
+
+;; (use-package ghc
+;;   :ensure t
+;;   :init (add-hook 'haskell-mode-hook 'ghc-init))
+
+;; (use-package company-ghc
+;;   :ensure t
+;;   :init (add-to-list 'company-backends 'company-ghc))
 
 (use-package flycheck-haskell
   :commands flycheck-haskell-setup)
