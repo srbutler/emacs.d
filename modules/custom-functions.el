a24f1b8d357457f5e5b0bb7ebccee6788be3934d
srbutler <srbutler@gmail.com>
2017-02-21 15:29:02 -0800

    Highlight certain comment notes

--- modules/custom-functions.el
+++ modules/custom-functions.el
@@ -18,7 +18,18 @@
 ;; open kasr1 from out of office
 (defun connect-kasr1-outside ()
     (interactive)
-  (dired "/office.kitsys.net:/home/srbutler/"))
+    (dired "/office.kitsys.net:/home/srbutler/"))
+
+;; add highlighting for general annotations
+;; taken from emacs-prelude
+(defun font-lock-comment-annotations ()
+  "Highlight a bunch of well known comment annotations.
+This functions should be added to the hooks of major modes for
+programming."
+  (font-lock-add-keywords
+   nil '(("\\<\\(\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):\\)"
+          1 font-lock-warning-face t))))
+(add-hook 'prog-mode-hook 'font-lock-comment-annotations)
 
 (provide 'custom-fuctions)
 ;;; custom-functions.el ends here
