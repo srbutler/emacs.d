b0f98f42895c2cb531918ea38d68119508bfc07b
Steven Butler <srbutler@gmail.com>
2017-03-31 10:14:02 -0400

    Updates

--- custom.el
+++ custom.el
@@ -3,9 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-gtags--label-option "pygments")
+ '(helm-gtags-auto-update t)
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-path-style (quote relative))
+ '(helm-gtags-prefix-key "C-c C-t")
+ '(helm-gtags-suggested-key-mapping t)
  '(package-selected-packages
    (quote
-    (helm-gtags zenburn-theme yaml-mode wrap-region which-key web-mode use-package typescript-mode swift-mode sr-speedbar solarized-theme smartparens smart-mode-line slime skewer-mode rustfmt rtags rainbow-mode rainbow-delimiters racer pyenv-mode pcre2el pandoc-mode pallet org-plus-contrib mmm-mode material-theme markdown-mode magit leuven-theme key-chord json-mode js2-refactor js-comint irony-eldoc intero idle-highlight-mode helm-projectile helm-descbinds helm-ag go-eldoc git-gutter geiser flycheck-stack flycheck-rust flycheck-pos-tip flycheck-irony flycheck-hdevtools flycheck-haskell flycheck-clojure flycheck-cask flatland-theme exec-path-from-shell ess erlang emmet-mode elpy elixir-mode dash-at-point darkokai-theme cython-mode csv-mode crux company-tern company-racer company-irony-c-headers company-irony company-go company-ghc color-theme-sanityinc-tomorrow coffee-mode cmake-mode cmake-ide clj-refactor clang-format cdlatex cargo avy auctex anti-zenburn-theme))))
+    (gnuplot-mode org-bullets zenburn-theme yaml-mode wrap-region which-key web-mode use-package typescript-mode swift-mode sr-speedbar solarized-theme smartparens smart-mode-line slime skewer-mode rtags rainbow-mode rainbow-delimiters racer pyenv-mode pcre2el pandoc-mode pallet org-plus-contrib mmm-mode material-theme markdown-mode magit leuven-theme key-chord json-mode js2-refactor js-comint irony-eldoc intero idle-highlight-mode helm-projectile helm-gtags helm-descbinds helm-ag go-projectile git-gutter geiser flycheck-stack flycheck-rust flycheck-pos-tip flycheck-irony flycheck-hdevtools flycheck-haskell flycheck-clojure flycheck-cask flatland-theme exec-path-from-shell ess erlang emmet-mode elpy elixir-mode dash-at-point darkokai-theme cython-mode csv-mode crux company-tern company-racer company-irony-c-headers company-irony company-go company-ghc color-theme-sanityinc-tomorrow coffee-mode cmake-mode cmake-ide clj-refactor clang-format cdlatex cargo avy auctex anti-zenburn-theme))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
