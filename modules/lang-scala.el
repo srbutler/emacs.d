094e1233e09ff56bc1c939bdacfa403fd61b5e63
Steven Butler <srbutler@gmail.com>
2018-01-19 22:43:09 -0500

    Add Scala module

---
+++ modules/lang-scala.el
@@ -0,0 +1,41 @@
+;;; lang-scala.el --- Summary:
+;;
+;;; Commentary:
+;;  A lot is taken from http://ensime.github.io/editors/emacs/hacks/
+;;
+;;; Code:
+
+(use-package scala-mode
+  :ensure t
+  :pin melpa
+  :config
+
+  ;; better newline handling in comments
+  (defun scala-mode-newline-comments ()
+    "Custom newline appropriate for `scala-mode'."
+    ;; shouldn't this be in a post-insert hook?
+    (interactive)
+    (newline-and-indent)
+    (scala-indent:insert-asterisk-on-multiline-comment))
+  (bind-key "RET" 'scala-mode-newline-comments scala-mode-map)
+
+  ;; smartparens conditional whitespace padding
+  (sp-local-pair 'scala-mode "(" nil
+                 :post-handlers '(("||\n[i]" "RET")))
+  (sp-local-pair 'scala-mode "{" nil
+                 :post-handlers '(("||\n[i]" "RET") ("| " "SPC")))
+
+  ;; skips headers, goes to go when opening a file
+  (scala-mode:goto-start-of-code))
+
+(use-package ensime
+  :ensure t
+  :pin melpa
+  :init (add-hook 'scala-mode-hook 'ensime-mode))
+
+(use-package sbt-mode
+  :ensure t
+  :pin melpa)
+
+(provide 'lang-scala)
+;;; lang-scala.el ends here
