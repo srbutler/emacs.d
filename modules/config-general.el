fef8088a933eb97f93e99746ce79f14325bbdf22
srbutler <srbutler@gmail.com>
2018-12-01 15:20:26 -0500

    Combine a few config files

--- modules/config-general.el
+++ modules/config-general.el
@@ -92,6 +92,10 @@
         try-complete-lisp-symbol-partially
         try-complete-lisp-symbol))
 
+
+;; used in a few places to define keybindings easily
+(use-package bind-key)
+
 ;; auto-indent on Enter
 ;; bind to prog-mode-map for now to not break helm, etc.
 (bind-key "<return>" #'newline-and-indent prog-mode-map)
@@ -125,6 +129,15 @@
 (add-hook 'prog-mode-hook 'electric-indent-mode)
 
 
+;; edit zsh/prezto files in sh-mode
+(defvar pretzo-files '("zlogin" "zlogout" "zpretzorc"
+                       "zprofile" "zshenv" "zshrc"))
+(mapc
+ (lambda (file)
+   (add-to-list 'auto-mode-alist `(,(format "\\%s\\'" file) . sh-mode)))
+ pretzo-files)
+
+
 ;; revert buffers automatically when underlying files are changed externally
 (use-package autorevert
   :diminish (auto-revert-mode . "ar")
@@ -136,8 +149,35 @@
   :ensure t)
 
 
-;; used in a few places to define keybindings easily
-(use-package bind-key)
+;; autocompletion with company
+(use-package company
+  :ensure t
+  :diminish (company-mode . "comp")
+  :custom
+  (company-idle-delay 0.5)
+  (company-tooltip-limit 10)
+  (company-minimum-prefix-length 2)
+  (company-selection-wrap-around t)
+  (company-tooltip-flip-when-above t)
+  :config
+  (global-company-mode 1)
+
+  ;; remap select-next/prev to use normal up/down
+  (with-eval-after-load 'company
+    (define-key company-active-map (kbd "M-n") nil)
+    (define-key company-active-map (kbd "M-p") nil)
+    (define-key company-active-map (kbd "C-n") #'company-select-next)
+    (define-key company-active-map (kbd "C-p") #'company-select-previous)))
+
+
+;; rank completions based on usage
+(use-package company-statistics
+  :ensure t
+  :after company
+  :custom
+  (company-statistics-file
+   (expand-file-name "company-statistics-cache.el" *savefile-dir*))
+  :config (company-statistics-mode))
 
 
 ;; set up some of crux's convenience functions
@@ -164,15 +204,124 @@
   (crux-with-region-or-buffer tabify))
 
 
+(use-package csv-mode
+  :ensure t
+  :mode (("\\.csv\\'" . csv-mode)
+         ("\\.tsv\\'" . csv-mode)))
+
+
+;; set up dash integration
+(use-package dash-at-point
+  :disabled t
+  :ensure t
+  :defer t
+  :config
+  (dolist
+      (pair
+       '('(python-mode . "python3") '(sh-mode . "bash") '(emacs-lisp-mode . "elisp")
+         '(LaTeX-mode . "latex") '(js2-mode . "javascript") '(tuareg-mode . "ocaml")
+         '(ess-mode . "r")))
+    (add-to-list 'dash-at-point-mode-alist pair)))
+
+
 ;; diminish keeps the modeline tidy
 (use-package diminish
   :ensure t)
+
+
+(use-package dockerfile-mode
+  :ensure t
+  :mode ("Dockerfile\\'" . dockerfile-mode))
+
+
+
+;; display certain documentation in the minibuffer
+(use-package eldoc-mode
+  :ensure nil
+  :diminish ""
+  :hook prog-mode
+  :config
+  ;; give current argument distinctive highlighting
+  (set-face-attribute 'eldoc-highlight-function-argument nil
+                      :underline t
+                      :foreground (face-foreground font-lock-constant-face)
+                      :weight 'bold))
+
 
 
 ;; expands the selection region progressively
 (use-package expand-region
   :ensure t
   :bind ("C-=" . er/expand-region))
+
+
+;; https://www.masteringemacs.org/article/working-multiple-files-dired
+(use-package find-file
+  :init (setq find-ls-option '("-print0 | xargs -0 ls -ld" . "-ld")))
+
+
+;; syntax-checking
+(use-package flycheck
+  :ensure t
+  :ensure-system-package (cppcheck shellcheck)
+  :diminish (flycheck-mode . "flyc")
+  :config
+  (global-flycheck-mode)
+
+  ;; constant rechecking gets annoying
+  (setq flycheck-check-syntax-automatically '(mode-enabled save new-line))
+
+  ;; get rid of annoying contrasts when using certain themes
+  (set-face-background 'flycheck-fringe-info nil)
+  (set-face-background 'flycheck-fringe-error nil)
+  (set-face-background 'flycheck-fringe-warning nil))
+
+
+;; have git indications in gutter
+(use-package git-gutter
+  :ensure t
+  :init (global-git-gutter-mode t)
+  :diminish git-gutter-mode
+  :config
+  ;; change the indicator colors to something nicer
+  (when (or (eq *current-theme-name* "solarized-dark")
+            (eq *current-theme-name* "solarized-light"))
+    (progn
+      (set-face-foreground 'git-gutter:added "#859900")
+      (set-face-foreground 'git-gutter:deleted "#dc322f")
+      (set-face-foreground 'git-gutter:modified "#b58900"))))
+
+
+;; navigate through git commit history
+(use-package git-timemachine
+  :ensure t
+  :defer t)
+
+
+;; major mode for .gitconfig files
+(use-package gitattributes-mode
+  :ensure t
+  :defer t)
+
+
+;; major mode for .gitconfig files
+(use-package gitconfig-mode
+  :ensure t
+  :defer t)
+
+
+;; major mode for .gitignore files
+(use-package gitignore-mode
+  :ensure t
+  :defer t)
+
+
+(use-package gnuplot-mode
+  :ensure t
+  :mode (("\\.gpi\\'" . gnuplot-mode)
+         ("\\.plt\\'" . gnuplot-mode)
+         ("\\.gp\\'"  . gnuplot-mode)))
+
 
 
 ;; define a bunch of quick key combos for basic actions
@@ -187,16 +336,84 @@
   (key-chord-define-global "jk" 'avy-goto-char))
 
 
+;; set up magit for git
+(use-package magit
+  :ensure t
+  :defer t
+  :custom (magit-completing-read-function 'ivy-completing-read)
+  :bind (("C-c g l"   . magit-log-popup)
+         ("C-c g p s" . magit-push-popup)
+         ("C-c g p l" . magit-pull-and-fetch-popup)
+         ("C-c g r"   . magit-rebase-popup)
+         ("C-c g s"   . magit-status)))
+
+
+;; display TODOs in status buffer
+(use-package magit-todos
+  :ensure t
+  :hook (magit-mode . magit-todos-mode))
+
+
+;; edit with multiple cursors
+(use-package multiple-cursors
+  :ensure t
+  :config (setq mc/list-file (expand-file-name "mc-lists.el" *savefile-dir*))
+  :bind
+   (("C-c m t" . mc/mark-all-like-this)
+    ("C-c m m" . mc/mark-all-like-this-dwim)
+    ("C-c m l" . mc/edit-lines)
+    ("C-c m e" . mc/edit-ends-of-lines)
+    ("C-c m a" . mc/edit-beginnings-of-lines)
+    ("C-c m n" . mc/mark-next-like-this)
+    ("C-c m p" . mc/mark-previous-like-this)
+    ("C-c m s" . mc/mark-sgml-tag-pair)
+    ("C-c m d" . mc/mark-all-like-this-in-defun)))
+
+
+;; pandoc
+(use-package pandoc-mode
+  :ensure t
+  :ensure-system-package pandoc
+  :diminish (pandoc-mode . "pandoc")
+  :hook (markdown-mode org-mode TeX-mode)
+  :config (pandoc-load-default-settings))
+
+
+;; minor-mode and utility for regex conversion (perl <--> elisp)
+(use-package pcre2el
+  :ensure t
+  :diminish (pcre-mode . "pcre")
+  :init (pcre-mode +1))
+
+
 ;; workspace management
 (use-package perspective
   :ensure t
   :config (persp-mode))
 
 
+;; project management and fast-switching
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :config
+  (projectile-mode t)
+  (setq projectile-cache-file
+        (expand-file-name  "projectile.cache" *savefile-dir*))
+  (setq projectile-known-projects-file
+        (expand-file-name "projectile-bookmarks.eld" *savefile-dir*)))
+
+
+;; makes parentheses colorful
+(use-package rainbow-delimiters-mode
+  :ensure rainbow-delimiters
+  :hook (prog-mode))
+
+
 ;; displays colors for color hex values
 (use-package rainbow-mode
   :ensure t
-  :hook (emacs-lisp-mode css-mode)
+  :hook (emacs-lisp-mode css-mode conf-xdefaults-mode)
   :diminish rainbow-mode)
 
 
@@ -229,6 +446,28 @@
         ;; keep the home clean
         savehist-file (expand-file-name "savehist" *savefile-dir*))
   (savehist-mode +1))
+
+
+;; get smartparens in programming modes
+(use-package smartparens
+  :ensure t
+  :diminish (smartparens-mode . "sp")
+  :init
+  (use-package smartparens-config)
+  (sp-use-paredit-bindings)
+  (add-hook 'prog-mode-hook 'smartparens-strict-mode)
+  (show-smartparens-global-mode 1)
+  :config
+  (setq sp-base-key-bindings 'paredit
+        sp-autoskip-closing-pair 'always
+        sp-hybrid-kill-entire-symbol nil)
+
+  (sp-local-pair '(markdown-mode gfm-mode) "*" "*"
+                 :unless '(sp-in-string-p)
+                 :actions '(insert wrap))
+  :config
+  ;; conflicts with xref
+  (define-key smartparens-mode-map (kbd "M-?") nil))
 
 
 ;; better line-by-line scrolling, especially in terminals
@@ -321,6 +560,35 @@
   :ensure t
   :diminish ws-butler-mode
   :config (ws-butler-global-mode))
+
+
+;; ensure the initial xref commands are correctly bound
+(use-package xref
+  :bind (("M-." . xref-find-definitions)
+         ("M-?" . xref-find-references)
+         ("M-/" . xref-find-references)))
+
+
+(use-package yaml-mode
+  :ensure t
+  :mode (("\\.yml\\'" . yaml-mode)
+         ("\\.yaml\\'" . yaml-mode))
+  :commands yaml-mode
+  :config (add-hook 'yaml-mode-hook 'subword-mode))
+
+
+;; enable YASnippet globally
+(use-package yasnippet
+  :ensure t
+  :init (yas-global-mode)
+  :bind (("C-c C-e" . yas-expand))
+  :config (add-to-list 'yas-snippet-dirs
+                       (expand-file-name "snippets/" *dotfiles-dir*)))
+
+
+;; a solid collection of snippets for many modes
+(use-package yasnippet-snippets
+  :ensure t)
 
 
 (provide 'config-ui)
